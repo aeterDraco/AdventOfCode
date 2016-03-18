afe3c95a4264222a35b66912b87ff740beb92d5a
Ricardo Quintero <ricardo.quintero.botia@schibsted.hu>
2016-03-18 21:56:32 +0100

    Haskell day1 modified

--- Haskell/Day1/day1.hs
+++ Haskell/Day1/day1.hs
@@ -1,7 +1,6 @@
 import Data.Char
 import Data.Maybe
 import Data.List
-import Data.Map hiding (map)
 import Text.Printf
 
 stripChars :: String -> String -> String
@@ -10,5 +9,6 @@ stripChars = filter . flip notElem
 main = do
 	src <- readFile "day1.input"
 	let up = stripChars ")" src
-	putStrLn $ "" ++ src
+	let down = stripChars "(" src
+	putStrLn $ "" ++ up - down
 
